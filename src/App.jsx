81eb0f476970745806c2da6508cdd8a75cec81ae
Bathini Nipun <116077457+bathini79@users.noreply.github.com>
2024-07-07 16:28:36 +0000

    feat:CRUD Basic Todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,46 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState()
+  const [todo, setTodo] = useState("")
+  const [todoIndex, setTodoIndex] = useState("")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <>
+        <h2>Todo Application</h2>
+        <input onChange={(e) => setTodo(e.target.value)} value={todo} />  <button style={{ marginLeft: '4px' }}
+          onClick={() => {
+            if (!String(todoIndex)) {
+              if (todos?.length >= 1) {
+                setTodos([...todos, todo])
+              } else {
+                setTodos([todo])
+              }
+            }
+            else {
+              let _todos = todos
+              _todos[todoIndex] = todo
+              setTodoIndex("")
+              setTodos(_todos)
+            }
+            setTodo("")
+          }}
+        >{String(todoIndex) ? <span>Update</span> : <span>Add</span>}</button>
+        {todos && todos?.map((todoData, index) => (
+          <div key={index}>
+            {todoData}
+            <button onClick={() => {
+              let _todos = todos.filter((todo, todoIndex) => todoIndex != index)
+              setTodos(_todos)
+            }}>Delete</button>
+            <button onClick={() => {
+              setTodoIndex(index)
+              setTodo(todos[index])
+            }}>Edit</button>
+          </div>
+        ))}
+      </>
     </div>
   );
 }
